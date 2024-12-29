ed1af0e987a2231e9ecbc4c621129c46b78dbb7d
VvSsNPK <praveenpp2929@gmail.com>
2024-12-29 20:23:14 +0100

    btree

---
+++ btree.lean
@@ -0,0 +1,21 @@
+structure Btree where
+  val : Option Int
+  left : Option Btree
+  right : Option Btree
+
+def btre : Btree := {val := none, left:=none, right:=none}
+
+def Btree.MkBtree (α : Option Int) : Btree :=
+  {val := α , left:= none,right:= none}
+
+def Btree.Insert (α : Int) (β : Btree) : Btree :=
+  match β.val with
+  | none => {val := some α,left := none,right:= none}
+  | some x => if x >= α then
+    match β.left with
+    | some y => {val:= some x,left := Insert α y,right:=none}
+    | none => {val := some α,left := none,right:=none}
+    else 
+    match β.right with
+    | some y => {val:= some x,left:= none,right:=Insert α y}
+    | none => {val:= some α,left:=none,right:=none}
